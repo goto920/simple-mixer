a4fbc70a61e6f4fbefff2b786e7b7519780d4dde
goto920 <goto@kmgoto.jp>
2021-03-17 06:25:42 +0900

    Prepare to switch App and AppNoWorklet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppNoWorklet from './AppNoWorklet';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
@@ -13,12 +14,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 */
-
-ReactDOM.render(
-    <App />,
+  ReactDOM.render(
+   <AppNoWorklet />,
   document.getElementById('root')
-);
-
+  );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
