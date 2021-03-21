d09ca2a7b7a018ff9ce26f2e5cd13fdea9a002a2
goto920 <goto@kmgoto.jp>
2021-03-22 07:09:39 +0900

    shoudlComponentUpdate() instead of componentDidupdate()

--- src/jslibs/PlayButton.js
+++ src/jslibs/PlayButton.js
@@ -9,51 +9,46 @@ import PauseCircleOutlineOutlinedIcon
 export default class PlayButton extends Component {
   constructor(props){
     super();
-    this.state = {
-      nextAction: props.nextAction,
-      m : props.messages,
-    };
-    this.handler = props.handler;
   }
 
-componentDidUpdate(prevProps) {
-  if (prevProps.nextAction !== this.props.nextAction) {
-    this.setState({nextAction: this.props.nextAction}); 
-  }
-  if (prevProps.messages !== this.props.messages) {
-    this.setState({m: this.props.messages}); 
-  }
-}
+// No setState() in shouldComponentUpdate
+  shouldComponentUpdate(nextProps, nextState){
+    if (nextProps.nextAction !== this.props.nextAction
+     || nextProps.messages !== this.props.messages)
+    return true;
 
-// PlayButton 
-  render(){
-    const {nextAction,m} = this.state;
+    return false;
+  }
+
+  render(){ // no state change
+
+    const {nextAction,messages,handler} = this.props;
 
     let icon = null;
     switch(nextAction){
          case 'load files first!':
            icon = 
-             <Tooltip title={m.alert}>
+             <Tooltip title={messages.alert}>
              <span><IconButton
-             onClick={() => this.handler({target: {name: 'startPause'}})} >
+             onClick={() => handler({target: {name: 'startPause'}})} >
              <PlayCircleOutlineIcon color='disabled'/>
              </IconButton></span></Tooltip>;
          break;
          case 'Play': 
-           icon = <Tooltip title={m.playButton}><IconButton  
-             onClick={() => this.handler({target: {name: 'startPause'}})} >
+           icon = <Tooltip title={messages.playButton}><IconButton  
+             onClick={() => handler({target: {name: 'startPause'}})} >
              <PlayCircleOutlineIcon color='primary' />
              </IconButton></Tooltip>;
          break;
          case 'Resume':
            icon = <IconButton  
-             onClick={() => this.handler({target: {name: 'startPause'}})} >
+             onClick={() => handler({target: {name: 'startPause'}})} >
              <PlayCircleOutlineIcon style={{color: '#00aa00' }} />
              </IconButton>;
          break;
          case 'Pause': 
            icon = <IconButton  
-             onClick={() => this.handler({target: {name: 'startPause'}})} >
+             onClick={() => handler({target: {name: 'startPause'}})} >
              <PauseCircleOutlineOutlinedIcon color='primary' />
              </IconButton>;
          break;
